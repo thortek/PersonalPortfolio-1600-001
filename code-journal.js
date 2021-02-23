46dc102e91672ec6274892849ef20850fd014c75
Thor Anderson <ta.anderson@gmail.com>
2021-02-23 11:14:15 -0700

    Just added some sample JavaScript

---
+++ code-journal.js
@@ -0,0 +1,36 @@
+/* Variables - containers that store values */
+
+var name // a declared variable, but not initialized and it's in the global scope (BAD)
+
+let foo // a declared variable that can be changed
+
+const bar // a declared variable that cannot be changed - short for 'constant'
+
+const ANSWER = 42 // const is declared and initialized with the value 42 
+
+// String
+
+let string1 = 'Hello World!' // read '=' as 'is assigned the value of...'
+
+let string2 = "Hello Utah!"
+
+let string3 = new String('Hello World!')
+
+// Number
+
+let myNum = 2083972347
+
+let myNum2 = 75.43
+
+'1' == 1 // this statement is true because of type coercion and loose equality checking
+'1' === 1 // false because this is strict equality checking
+
+// Boolean
+
+let myBool = true
+
+// Array
+
+let myArray = [] // this is an empty array
+
+let myArray2 = [42, 'Bob', myBool, ANSWER, true]
