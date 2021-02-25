0faa42873d75c979ac861a452639b42914902ff9
Thor Anderson <ta.anderson@gmail.com>
2021-02-25 11:14:03 -0700

    Added for loop

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -8,3 +8,7 @@ let filmTwo = document.querySelector('#film2')
 
 filmOne.textContent = films[2].title
 filmTwo.textContent = films[1].title
+
+for (var i = 0; i < films.length; i++) {
+    console.log(films[i]);
+ }
