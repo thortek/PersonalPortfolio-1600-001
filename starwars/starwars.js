8019ca404ca147e66a4edc0f2fdc62f1ce9bb927
Thor Anderson <ta.anderson@gmail.com>
2021-03-02 11:14:47 -0700

    Added film posters

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -7,9 +7,16 @@ let filmList = document.querySelector('#filmList')
 for (let i = 0; i < films.length; i++) {
 
     const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
-    let filmItem = document.createElement('li')
-    filmItem.textContent = foundFilm.title
-    filmList.appendChild(filmItem)
+    let posterFig = document.createElement('figure')
+    let figImg = document.createElement('img')
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+    let figCaption = document.createElement('figcaption')
+
+    figCaption.textContent = foundFilm.title
+    posterFig.appendChild(figImg)
+    posterFig.appendChild(figCaption)
+
+    filmList.appendChild(posterFig)
 }
  
 function getLastNumber(url) {
