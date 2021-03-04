e23ee6fef1b7a3a42a8ed170573074f850c2af51
Thor Anderson <ta.anderson@gmail.com>
2021-03-04 11:10:23 -0700

    Added male and female character filters

--- characters/main.js
+++ characters/main.js
@@ -2,19 +2,34 @@ import { people } from '../data/people.js'
 
 const mainElement = document.querySelector('#main')
 
-people.forEach((person) => {
-    const charFigure = document.createElement('figure')
-    const charImg = document.createElement('img')
-    let charNum = getLastNumber(person.url)
-    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
-    const charCaption = document.createElement('figcaption')
-    charCaption.textContent = person.name
+const mainHeader = document.createElement('header')
 
-    charFigure.appendChild(charImg)
-    charFigure.appendChild(charCaption)
+const maleButton = document.createElement('button')
+maleButton.textContent = 'Male Characters'
+mainHeader.appendChild(maleButton)
 
-    mainElement.appendChild(charFigure)
-})
+document.body.insertBefore(mainHeader, mainElement)
+
+const maleCharacters = people.filter(person => person.gender === 'male')
+const femaleCharacters = people.filter(person => person.gender === 'female')
+
+populateDOM(femaleCharacters)
+
+function populateDOM(characters) {
+    characters.forEach((person) => {
+        const charFigure = document.createElement('figure')
+        const charImg = document.createElement('img')
+        let charNum = getLastNumber(person.url)
+        charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+        const charCaption = document.createElement('figcaption')
+        charCaption.textContent = person.name
+
+        charFigure.appendChild(charImg)
+        charFigure.appendChild(charCaption)
+
+        mainElement.appendChild(charFigure)
+    })
+}
 
 function getLastNumber(url) {
     let end = url.lastIndexOf('/')
@@ -24,3 +39,4 @@ function getLastNumber(url) {
     }
     return url.slice(start, end)
 }
+
