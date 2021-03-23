9a81f2126b142f1c02da7ea35cbe438244dcd3c0
Thor Anderson <ta.anderson@gmail.com>
2021-03-23 11:27:08 -0600

    Added starfield to starships page

--- starships/main.js
+++ starships/main.js
@@ -34,10 +34,28 @@ function populateShipView(shipData) {
 
 function addStarField(element, numStars) {
     element.style.setProperty('background-color', '#000')
+    for (let i = 0; i < numStars; i++) {
+        let star = document.createElement('div')
+        star.style.setProperty('position', 'absolute')
+        star.style.setProperty('width', '2px')
+        star.style.setProperty('height', '2px')
+        star.style.setProperty('background-color', 'white')
+        let xy = getRandomPosition()
+        star.style.left = `${xy[0]}px`
+        star.style.top = `${ xy[1]}px`
+        element.appendChild(star)
+    }
 }
 
-addStarField(document.querySelector('body'), 1000)
+function getRandomPosition() {
+    let y = document.body.scrollHeight
+    let x = document.body.scrollWidth
+    let randomY = Math.floor(Math.random() * y)
+    let randomX = Math.floor(Math.random() * x)
+    return [randomX, randomY]
+}
 
 populateNav(starships)
 
-populateShipView()
+addStarField(document.querySelector('body'), 1000)
+
