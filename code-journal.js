7dfee2ae52daaa02a5191dcaaab8bff1baea8844
Thor Anderson <ta.anderson@gmail.com>
2021-03-25 11:14:49 -0600

    Added congress data and code journal entries

--- code-journal.js
+++ code-journal.js
@@ -86,3 +86,53 @@ const myCar = {
 const theFunction = () => { //multiple lines use curly braces and 'return' keyword
   return 'I am awesome'
 }
+
+// Filter method example.  Filter returns an array of all elements that 'pass the test'
+const pilots = [
+  {
+    id: 2,
+    name: "Wedge Antilles",
+    faction: "Rebels"
+  },
+  {
+    id: 8,
+    name: "Ciena Ree",
+    faction: "Empire"
+  },
+  {
+    id: 40,
+    name: "Iden Versio",
+    faction: "Empire"
+  },
+  {
+    id: 66,
+    name: "Thane Kyrell",
+    faction: "Rebels"
+  }
+];
+
+const rebels = pilots.filter((pilot) => pilot.faction === "Rebels");
+const empire = pilots.filter((pilot) => {
+  return pilot.faction === "Empire";
+});
+
+// Array helper method 'map'
+
+let filmURLs = [
+  "https://swapi.co/api/films/",
+  "https://swapi.co/api/films/5/",
+  "https://swapi.co/api/films/4/this one is longer... even longer",
+  "https://swapi.co/api/films/6/",
+  "https: ",
+  "https://swapi.co/api/films/1/"
+];
+
+const filmLengths = filmURLs.map((filmURL) => filmURL.length);
+
+const filmPlusMore = filmURLs.map((filmURL) => {
+  let filmObj = {
+    index: filmURL,
+    foo: 'Bar'
+  }
+  return filmObj
+})
---
+++ congress/congress.js
@@ -0,0 +1,4 @@
+import { senators } from '../data/senators.js'
+import { representatives } from '../data/representatives.js'
+
+console.log(senators.length, representatives.length)
