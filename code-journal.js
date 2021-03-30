8c83b501c78c6debb8aae8eeee0cd204e93d67f5
Thor Anderson <ta.anderson@gmail.com>
2021-03-30 11:13:34 -0600

    Got simplified data + images working

--- code-journal.js
+++ code-journal.js
@@ -136,3 +136,97 @@ const filmPlusMore = filmURLs.map((filmURL) => {
   }
   return filmObj
 })
+
+// Reduce example
+
+const swpilots = [
+  {
+    id: 10,
+    name: "Poe Dameron",
+    years: 14
+  },
+  {
+    id: 2,
+    name: "Temmin 'Snap' Wexley",
+    years: 30
+  },
+  {
+    id: 41,
+    name: "Tallissan Lintra",
+    years: 16
+  },
+  {
+    id: 99,
+    name: "Ello Asty",
+    years: 22
+  }
+];
+
+const totalYears = swpilots.reduce((acc, pilot) => acc + pilot.years, 0);
+
+const mostExpPilot = swpilots.reduce((oldest, pilot) => {
+  return (oldest.years || 0) > pilot.years ? oldest : pilot;
+}, {});
+
+// Now we combine map, reduce, and filter
+
+var personnel = [
+  {
+    id: 5,
+    name: "Luke Skywalker",
+    pilotingScore: 98,
+    shootingScore: 56,
+    isForceUser: true
+  },
+  {
+    id: 82,
+    name: "Sabine Wren",
+    pilotingScore: 73,
+    shootingScore: 99,
+    isForceUser: false
+  },
+  {
+    id: 22,
+    name: "Zeb Orellios",
+    pilotingScore: 20,
+    shootingScore: 59,
+    isForceUser: false
+  },
+  {
+    id: 15,
+    name: "Ezra Bridger",
+    pilotingScore: 43,
+    shootingScore: 67,
+    isForceUser: true
+  },
+  {
+    id: 11,
+    name: "Caleb Dume",
+    pilotingScore: 71,
+    shootingScore: 85,
+    isForceUser: true
+  }
+];
+
+let jediPersonnel = personnel.filter((person) => person.isForceUser);
+
+let jediScores = jediPersonnel.map(
+  (jedi) => jedi.pilotingScore + jedi.shootingScore
+);
+
+let totalJediScore = jediScores.reduce((acc, score) => acc + score, 0);
+
+const totalJediScoreChained = personnel
+  .filter((person) => person.isForceUser)
+  .map((jedi) => jedi.pilotingScore + jedi.shootingScore)
+  .reduce((acc, score) => acc + score, 0);
+
+// Ternary operator syntax: condition ? exprIfTrue : exprIfFalse
+
+const totalJediScoreReduce = personnel.reduce(
+  (acc, person) =>
+    person.isForceUser
+      ? acc + person.pilotingScore + person.shootingScore
+      : acc,
+  0
+);
--- congress/congress.js
+++ congress/congress.js
@@ -1,4 +1,45 @@
 import { senators } from '../data/senators.js'
 import { representatives } from '../data/representatives.js'
 
-console.log(senators.length, representatives.length)
+const congressGrid = document.querySelector('.congressGrid')
+const seniorityButton = document.querySelector('#seniorityButton')
+const birthdayButton = document.querySelector('#birthdayButton')
+
+seniorityButton.addEventListener('click', () => {
+    senioritySort()
+})
+
+birthdayButton.addEventListener('click', () => {
+    birthdaySort()
+})
+
+function populateCongressGrid(simplePeople) {
+    console.log(simplePeople)
+    simplePeople.forEach(person => {
+        let personDiv = document.createElement('div')
+        let personFig = document.createElement('figure')
+        let figImg = document.createElement('img')
+        let figCaption = document.createElement('figcaption')
+
+        figImg.src = person.imgURL
+        figCaption.textContent = `${person.name}`
+
+        personFig.appendChild(figImg)
+        personFig.appendChild(figCaption)
+        personDiv.appendChild(personFig)
+        congressGrid.appendChild(personDiv)
+    })
+}
+
+function getSimplifiedCongress(congressPeople) {
+    return congressPeople.map(person => {
+        let middleName = person.middle_name ? `${person.middle_name}` : ``
+        return {
+            id: person.id,
+            name: `${person.first_name} ${middleName} ${person.last_name}`,
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${person.govtrack_id}-100px.jpeg`
+        }
+    })
+}
+
+populateCongressGrid(getSimplifiedCongress(senators))
