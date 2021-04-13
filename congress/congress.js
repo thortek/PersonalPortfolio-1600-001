bffd7dca061191881a34e777c85f8b2eaead77ad
Thor Anderson <ta.anderson@gmail.com>
2021-04-13 11:11:50 -0600

    Added some reducers

--- congress/congress.js
+++ congress/congress.js
@@ -5,6 +5,8 @@ import { removeChildren } from '../utils/index.js'
 const congressGrid = document.querySelector('.congressGrid')
 const seniorityButton = document.querySelector('#seniorityButton')
 const birthdayButton = document.querySelector('#birthdayButton')
+const missedVotesButton = document.querySelector('#missedVotes')
+const partyHackButton = document.querySelector('#partyHack')
 
 seniorityButton.addEventListener('click', () => {
     senioritySort()
@@ -12,6 +14,15 @@ seniorityButton.addEventListener('click', () => {
 
 birthdayButton.addEventListener('click', () => {
     birthdaySort()
+})
+
+missedVotesButton.addEventListener('click', () => {
+    alert(`${missedVotesRep.name} missed votes ${missedVotesRep.missed_votes_pct}% of the time!`)
+})
+
+partyHackButton.addEventListener('click', () => {
+    console.log(partyHack)
+    //alert(partyHack)
 })
 
 function populateCongressGrid(simplePeople) {
@@ -38,10 +49,13 @@ function getSimplifiedCongress(congressPeople) {
         let middleName = person.middle_name ? `${person.middle_name}` : ``
         return {
             id: person.id,
+            title: person.title,
             name: `${person.first_name} ${middleName} ${person.last_name}`,
             imgURL: `https://www.govtrack.us/static/legislator-photos/${person.govtrack_id}-100px.jpeg`,
             seniority: parseInt(person.seniority, 10),
-            date_of_birth: parseInt(person.date_of_birth, 10)
+            date_of_birth: parseInt(person.date_of_birth, 10),
+            missed_votes_pct: person.missed_votes_pct,
+            votes_with_party_pct: person.votes_with_party_pct
         }
     })
 }
@@ -57,5 +71,9 @@ function birthdaySort() {
         (a, b) => a.date_of_birth - b.date_of_birth
     ))
 }
+
+const missedVotesRep = getSimplifiedCongress(representatives).filter((rep) => rep.title === 'Representative').reduce((acc, rep) => acc.missed_votes_pct > rep.missed_votes_pct ? acc : rep)
+
+const partyHack = getSimplifiedCongress(representatives).filter((rep) => rep.title === 'Representative').reduce((acc, rep) => acc.votes_with_party_pct > rep.votes_with_party_pct ? acc : rep)
 
 populateCongressGrid(getSimplifiedCongress(senators))
