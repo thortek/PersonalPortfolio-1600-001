63c4881b6c360455e4ac0060845678c28f2a7dce
Thor Anderson <ta.anderson@gmail.com>
2021-04-15 10:51:59 -0600

    Removed images, new pokemon class, pokeball

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,36 +1,40 @@
 const pokeGrid = document.querySelector('.pokeGrid')
 const loadButton = document.querySelector('.loadPokemon')
 const fetchButton = document.querySelector('#fetchSelectedPokemon')
+const newButton = document.querySelector('#newPokemon')
 
-const dialog = document.querySelector('.modal')
-const closeButton = document.querySelector('.modal-close')
-const modalBackground = document.querySelector('.modal-background')
-const submitButton = document.querySelector('#submitButton')
-
-closeButton.addEventListener('click', () => {
-    dialog.classList.toggle("is-active")
-})
-
-modalBackground.addEventListener('click', () => {
-    dialog.classList.toggle("is-active")
-})
-
-loadButton.addEventListener('click', () => {
-    loadPage()
+class Pokemon {
+    constructor(name, height, weight, abilities, moves) {
+        this.id = 900
+        this.name = name
+        this.height = height
+        this.weight = weight
+        this.abilities = abilities
+        this.moves = moves
+    }
+}
+  
+newButton.addEventListener('click', () => {
+    let pokeName = prompt('What is the name of your new Pokemon?')
+    let pokeHeight = prompt('What is the height of your Pokemon?')
+    let pokeWeight = prompt('Pokemon weight?')
+    let newPokemon = new Pokemon(
+        pokeName,
+        pokeHeight,
+        pokeWeight,
+        ['eat', 'sleep'],
+        ['study', 'game']
+    )
+    console.log(newPokemon)
+    populatePokeCard(newPokemon)
 })
 
 fetchButton.addEventListener('click', () => {
-    dialog.classList.toggle("is-active")
-    /* getAPIData(`https://pokeapi.co/api/v2/pokemon/25`).then(
-        (data) => {
-            populatePokeCard(data)
-        }
-    ) */
-})
-
-submitButton.addEventListener('click', () => {
-    let inputField = document.querySelector('.input')
-    inputValue = inputField.value
+    let pokeNameOrId = prompt("Enter Pokemon ID or Name:").toLowerCase()
+    console.log(pokeNameOrId)
+    getAPIData(`https://pokeapi.co/api/v2/pokemon/${pokeNameOrId}`).then(
+        (data) => populatePokeCard(data)
+    )
 })
 
 async function getAPIData(url) {
@@ -99,5 +103,8 @@ function getImageFileName(pokemon) {
     if (pokemon.id < 10) pokeId = `00${pokemon.id}`
     if (pokemon.id > 9 && pokemon.id < 100) pokeId = `0${pokemon.id}`
     if (pokemon.id > 99 && pokemon.id < 810) pokeId = pokemon.id
+    if (pokemon.id === 900) {
+        return `images/pokeball.png`
+    }
     return `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${pokeId}.png`
 }
